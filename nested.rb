a9aca947e1d8b6e8f403507657fe0c826c9e8c34
Michael Boroski <mjboroski@gmail.com>
2018-04-21 06:09:42 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -128,7 +128,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 thing=programmer_hash[:dennis_ritchie][:languages]
-		 thing<<"Assembly"
+
+   programmer_hash[:dennis_ritchie][:languages]<<"Assembly"
 
 end
